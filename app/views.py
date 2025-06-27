b9b30fb975bdf5268b0f2bbb5594f6caa3ffe147
ysadoragarcia <ysagdora@gmail.com>
2025-06-27 16:23:05 -0300

    ysa

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    #return HttpResponse("<h1> Ysa <h1/>")
+    return render(request, "teste.html")
